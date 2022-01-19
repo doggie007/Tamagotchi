5ae0015d54ad9c2d78e4776414fe8df50f7b6e6c
James Tcheng <james.tcheng1234@gmail.com>
2022-01-19 10:22:18 +0000

    Added colour to greeting message.

--- Tamagotchi/ContentView.swift
+++ Tamagotchi/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, universe!")
             .padding()
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
     }
 }
 
