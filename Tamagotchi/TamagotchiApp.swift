ced0851d21869d3f10be10203db1feb2914d1ed3
James Tcheng <james.tcheng1234@gmail.com>
2022-01-19 09:55:51 +0000

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Tcheng, James (BJH) on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
